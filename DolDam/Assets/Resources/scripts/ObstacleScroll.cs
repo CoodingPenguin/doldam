d6feac683759de5221db252738ba1f55f83bf4cc
HappyFaceFriend <yjl980730@gmail.com>
2018-07-08 12:38:46 +0900

    눈사람추가, 벽에 부딛힐때 이펙트 추가, 눈덩이크기와 속도 유기적으로 변경, 양쪽 벽 막음

---
+++ DolDam/Assets/Resources/scripts/ObstacleScroll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScroll : MonoBehaviour {
+
+    private GameManager gm;
+    private SpriteRenderer sr;
+    private float pixToUnit;
+    private float width;
+    private float height;
+    private bool isHit;
+	// Use this for initialization
+	void Awake ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        gm = GameManager.instance;
+        pixToUnit = GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        width = sr.sprite.rect.width / pixToUnit;
+        height = sr.sprite.rect.height / pixToUnit;
+        isHit = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gm.gameState == GameManager.GameState.PLAYING)
+        {
+            transform.Translate(new Vector2(0, -Time.deltaTime * gm.GetScrollSpeed()));
+            if (transform.position.y + height / 2 < -Screen.height / 2 / pixToUnit)
+                Destroy(gameObject);
+        }
+    }
+
+    public void HitByPlayer()
+    {
+        isHit = true;
+    }
+    public bool GetIsHit()
+    {
+        return isHit;
+    }
+}
---
+++ DolDam/Assets/Resources/scripts/ParticleScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScroll : MonoBehaviour {
+
+    GameManager gm;
+	// Use this for initialization
+	void Start ()
+    {
+        gm = GameManager.instance;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (gm.gameState == GameManager.GameState.PLAYING)
+        {
+            transform.Translate(new Vector2(0, -Time.deltaTime * gm.GetScrollSpeed())/2);
+        }
+    }
+}
