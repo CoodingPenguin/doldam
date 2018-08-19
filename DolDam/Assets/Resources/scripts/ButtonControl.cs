d85b0726f6a2323ce2a6a8f9e79f5312f25db8be
DESKTOP-NR49AC4\round <unodostre@naver.com>
2018-08-19 18:23:07 +0900

    뒤로가기 종료창 생성 및 글씨색깔/설정창/크레딧 수정

--- DolDam/Assets/Resources/scripts/ButtonControl.cs
+++ DolDam/Assets/Resources/scripts/ButtonControl.cs
@@ -11,12 +11,14 @@ public class ButtonControl : MonoBehaviour {
 
     public void LeftButtonOnClicked()
     {
+        Debug.Log("시작화면 - 왼쪽 버튼");
         SoundManager.instance.PlaySingleForBtn(leftButtonSound);
         SceneManager.LoadScene("Resources/scenes/DeveloperScene");
     }
 
     public void RightButtonOnClicked()
     {
+        Debug.Log("시작화면 - 오른쪽 버튼");
         SoundManager.instance.PlaySingleForBtn(rightButtonSound);
         SceneManager.LoadScene("Resources/scenes/SetUpScene");
 
@@ -24,9 +26,15 @@ public class ButtonControl : MonoBehaviour {
 
     public void StartButtonOnClicked()
     {
+        Debug.Log("시작화면 - 시작 버튼");
         SoundManager.instance.bgmSource.Stop();
         SoundManager.instance.PlaySingleForBtn(startButtonSound);
         SceneManager.LoadScene("Resources/scenes/GameScene");
         GameManager.instance.gameState = GameManager.GameState.PLAYING;
+    }
+
+    public void YesButtonOnClicked()
+    {
+        Application.Quit();
     }
 }
---
+++ DolDam/Assets/Resources/scripts/TurnOffScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffScript : MonoBehaviour {
+
+    public GameObject turnOffPanel;
+    public AudioClip btnSound;
+	
+	void Start () {
+        turnOffPanel = GameObject.FindGameObjectWithTag("TurnOff");
+        turnOffPanel.SetActive(false);
+	}
+	
+	
+	void Update () {
+		if(Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                turnOffPanel.SetActive(true);
+            }
+        }
+	}
+
+    public void YesButtonOnClicked()
+    {
+        SoundManager.instance.PlaySingleForBtn(btnSound);
+        Application.Quit();
+    }
+
+    public void NoButtonOnClicked()
+    {
+        SoundManager.instance.PlaySingleForBtn(btnSound);
+        turnOffPanel.SetActive(false);
+    }
+}
